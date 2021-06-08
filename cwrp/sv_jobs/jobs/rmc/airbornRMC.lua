d410048b8ae5c524e54aef3aac21723e0e8bf790
MKHalo <85412499+MKHalo@users.noreply.github.com>
2021-06-08 10:25:43 +0200

    Create airbornRMC.lua
    Neue untereinheit fürs RMC Airborn abgesprochen mit augu gemacht von Halo

---
+++ cwrp/sv_jobs/jobs/rmc/airbornRMC.lua
@@ -0,0 +1,183 @@
+----RMCAIRBORn-------
+
+TEAM_CLOASS = DarkRP.createJob("RMCJumptrooperPVT", {
+    color = Color(0, 0, 0, 255),
+    model = {"models/fisher/ctna/trooper/rmc_jump/rmc_jump_trooper.mdl", "models/DxN/cod_ghosts/hazmat_pm.mdl", "models/defcon/banks/coldwweather/74th_cold/74th_trooper/74th_trooper.mdl", "models/smitty/bf2_reg/medic_officer/medic_officer.mdl"},
+    description = [[RMCJumpPVT]],
+    weapons = {"cross_arms_swep", "zbl_spray", "zbl_gun", "cross_arms_infront_swep", "surrender_animation_swep", "salute_swep", "weapon_fists", "re_hands" ,"weapon_bactainjector" ,"weapon_medkit" ,"jet_mk1" ,"sw_datapad" ,"tfa_defi_swrp" ,"rw_sw_nade_bacta" ,"rw_ammo_distributor" ,"rw_sw_dc15a_o" ,"rw_sw_dc15s" ,"rw_sw_dc17"},
+    command = "RMCAirbornPVT",
+    max = 0,
+    salary = 100,
+    admin = 0,
+    vote = false,
+    hasLicense = true,
+    candemote = false,
+    category = "RMC",
+    PlayerSpawn = function(ply)
+        ply:SetMaxHealth(100)
+        ply:SetHealth(100)
+        ply:SetArmor(100)
+    end
+})
+
+TEAM_CLOASS = DarkRP.createJob("RMCJumptrooperPFC", {
+    color = Color(0, 0, 0, 255),
+    model = {"models/fisher/ctna/trooper/rmc_jump/rmc_jump_trooper.mdl", "models/DxN/cod_ghosts/hazmat_pm.mdl", "models/defcon/banks/coldwweather/74th_cold/74th_trooper/74th_trooper.mdl", "models/smitty/bf2_reg/medic_officer/medic_officer.mdl"},
+    description = [[RMCJumpPFC]],
+    weapons = {"cross_arms_swep", "zbl_spray", "zbl_gun", "cross_arms_infront_swep", "surrender_animation_swep", "salute_swep", "weapon_fists", "re_hands" ,"weapon_bactainjector" ,"weapon_medkit" ,"jet_mk1" ,"sw_datapad" ,"tfa_defi_swrp" ,"rw_sw_nade_bacta" ,"rw_ammo_distributor" ,"rw_sw_dc15a_o" ,"rw_sw_dc15s" ,"rw_sw_dc17"},
+    command = "RMCAirbornPFC",
+    max = 0,
+    salary = 100,
+    admin = 0,
+    vote = false,
+    hasLicense = true,
+    candemote = false,
+    category = "RMC",
+    PlayerSpawn = function(ply)
+        ply:SetMaxHealth(110)
+        ply:SetHealth(110)
+        ply:SetArmor(110)
+    end
+})
+
+
+
+TEAM_CLOASS = DarkRP.createJob("RMCJumptrooperLCPL", {
+    color = Color(0, 0, 0, 255),
+    model = {"models/fisher/ctna/trooper/rmc_jump/rmc_jump_trooper.mdl", "models/DxN/cod_ghosts/hazmat_pm.mdl", "models/defcon/banks/coldwweather/74th_cold/74th_trooper/74th_trooper.mdl", "models/smitty/bf2_reg/medic_officer/medic_officer.mdl"},
+    description = [[RMCJumpLCPL]],
+    weapons = {"cross_arms_swep", "zbl_spray", "zbl_gun", "cross_arms_infront_swep", "surrender_animation_swep", "salute_swep", "weapon_fists", "re_hands" ,"weapon_bactainjector" ,"weapon_medkit" ,"jet_mk1" ,"sw_datapad" ,"tfa_defi_swrp" ,"rw_sw_nade_bacta" ,"rw_ammo_distributor" ,"rw_sw_dc15a_o" ,"rw_sw_dc15s" ,"rw_sw_dc17"},
+    command = "RMCAirbornLCPL",
+    max = 0,
+    salary = 100,
+    admin = 0,
+    vote = false,
+    hasLicense = true,
+    candemote = false,
+    category = "RMC",
+    PlayerSpawn = function(ply)
+        ply:SetMaxHealth(120)
+        ply:SetHealth(120)
+        ply:SetArmor(120)
+    end
+})
+
+TEAM_CLOASS = DarkRP.createJob("RMCJumptrooperCPL", {
+    color = Color(0, 0, 0, 255),
+    model = {"models/fisher/ctna/trooper/rmc_jump/rmc_jump_trooper.mdl", "models/DxN/cod_ghosts/hazmat_pm.mdl", "models/defcon/banks/coldwweather/74th_cold/74th_trooper/74th_trooper.mdl", "models/smitty/bf2_reg/medic_officer/medic_officer.mdl"},
+    description = [[RMCJumpCPL]],
+    weapons = {"cross_arms_swep", "zbl_spray", "zbl_gun", "cross_arms_infront_swep", "surrender_animation_swep", "salute_swep", "weapon_fists", "re_hands" ,"weapon_bactainjector" ,"weapon_medkit" ,"jet_mk1" ,"sw_datapad" ,"tfa_defi_swrp" ,"rw_sw_nade_bacta" ,"rw_ammo_distributor" ,"rw_sw_dc15a_o" ,"rw_sw_dc15s" ,"rw_sw_dc17"},
+    command = "RMCAirbornCPL",
+    max = 0,
+    salary = 100,
+    admin = 0,
+    vote = false,
+    hasLicense = true,
+    candemote = false,
+    category = "RMC",
+    PlayerSpawn = function(ply)
+        ply:SetMaxHealth(130)
+        ply:SetHealth(130)
+        ply:SetArmor(130)
+    end
+})
+
+TEAM_CLOASS = DarkRP.createJob("RMCJumptrooperSGT", {
+    color = Color(0, 0, 0, 255),
+    model = {"models/fisher/ctna/trooper/rmc_jump/rmc_jump_trooper.mdl", "models/DxN/cod_ghosts/hazmat_pm.mdl", "models/defcon/banks/coldwweather/74th_cold/74th_trooper/74th_trooper.mdl", "models/smitty/bf2_reg/medic_officer/medic_officer.mdl"},
+    description = [[RMCJumpSGT]],
+    weapons = {"cross_arms_swep", "zbl_spray", "zbl_gun", "cross_arms_infront_swep", "surrender_animation_swep", "salute_swep", "weapon_fists", "re_hands" ,"weapon_bactainjector" ,"weapon_medkit" ,"jet_mk1" ,"sw_datapad" ,"tfa_defi_swrp" ,"rw_sw_nade_bacta" ,"rw_ammo_distributor" ,"rw_sw_dc15a_o" ,"rw_sw_dc15s" ,"rw_sw_dc17"},
+    command = "RMCAirbornSGT",
+    max = 0,
+    salary = 100,
+    admin = 0,
+    vote = false,
+    hasLicense = true,
+    candemote = false,
+    category = "RMC",
+    PlayerSpawn = function(ply)
+        ply:SetMaxHealth(140)
+        ply:SetHealth(140)
+        ply:SetArmor(140)
+    end
+})
+
+TEAM_CLOASS = DarkRP.createJob("RMCJumptrooperSGTMJ", {
+    color = Color(0, 0, 0, 255),
+    model = {"models/fisher/ctna/trooper/rmc_jump/rmc_jump_trooper.mdl", "models/DxN/cod_ghosts/hazmat_pm.mdl", "models/defcon/banks/coldwweather/74th_cold/74th_trooper/74th_trooper.mdl", "models/smitty/bf2_reg/medic_officer/medic_officer.mdl"},
+    description = [[RMCJumpSGTMJ]],
+    weapons = {"cross_arms_swep", "zbl_spray", "zbl_gun", "cross_arms_infront_swep", "surrender_animation_swep", "salute_swep", "weapon_fists", "re_hands" ,"weapon_bactainjector" ,"weapon_medkit" ,"jet_mk1" ,"sw_datapad" ,"tfa_defi_swrp" ,"rw_sw_nade_bacta" ,"rw_ammo_distributor" ,"rw_sw_dc15a_o" ,"rw_sw_dc15s" ,"rw_sw_dc17"},
+    command = "RMCAirbornSGTMJ",
+    max = 0,
+    salary = 100,
+    admin = 0,
+    vote = false,
+    hasLicense = true,
+    candemote = false,
+    category = "RMC",
+    PlayerSpawn = function(ply)
+        ply:SetMaxHealth(150)
+        ply:SetHealth(150)
+        ply:SetArmor(150)
+    end
+})
+
+TEAM_CLOASS = DarkRP.createJob("RMCJumptrooper2ndLT", {
+    color = Color(0, 0, 0, 255),
+    model = {"models/fisher/ctna/trooper/rmc_jump/rmc_jump_trooper.mdl", "models/DxN/cod_ghosts/hazmat_pm.mdl", "models/defcon/banks/coldwweather/74th_cold/74th_trooper/74th_trooper.mdl", "models/smitty/bf2_reg/medic_officer/medic_officer.mdl"},
+    description = [[RMCJump2ndLT]],
+    weapons = {"cross_arms_swep", "zbl_spray", "zbl_gun", "cross_arms_infront_swep", "rw_sw_dual_dc17", "weapon_cuff_elastic", "surrender_animation_swep", "salute_swep", "weapon_fists", "re_hands" ,"weapon_bactainjector" ,"weapon_medkit" ,"jet_mk1" ,"sw_datapad" ,"tfa_defi_swrp" ,"rw_sw_nade_bacta" ,"rw_ammo_distributor" ,"rw_sw_dc15a_o" ,"rw_sw_dc15s" ,"rw_sw_dc17"},
+    command = "RMCAirborn2ndLT",
+    max = 0,
+    salary = 100,
+    admin = 0,
+    vote = false,
+    hasLicense = true,
+    candemote = false,
+    category = "RMC",
+    PlayerSpawn = function(ply)
+        ply:SetMaxHealth(160)
+        ply:SetHealth(160)
+        ply:SetArmor(160)
+    end
+})
+
+TEAM_CLOASS = DarkRP.createJob("RMCJumptrooper1stLT", {
+    color = Color(0, 0, 0, 255),
+    model = {"models/fisher/ctna/trooper/rmc_jump/rmc_jump_trooper.mdl", "models/DxN/cod_ghosts/hazmat_pm.mdl", "models/defcon/banks/coldwweather/74th_cold/74th_trooper/74th_trooper.mdl", "models/smitty/bf2_reg/medic_officer/medic_officer.mdl"},
+    description = [[RMCJump1stLT]],
+    weapons = {"cross_arms_swep", "zbl_spray", "zbl_gun", "cross_arms_infront_swep", "rw_sw_dual_dc17", "weapon_cuff_elastic", "surrender_animation_swep", "salute_swep", "weapon_fists", "re_hands" ,"weapon_bactainjector" ,"weapon_medkit" ,"jet_mk1" ,"sw_datapad" ,"tfa_defi_swrp" ,"rw_sw_nade_bacta" ,"rw_ammo_distributor" ,"rw_sw_dc15a_o" ,"rw_sw_dc15s" ,"rw_sw_dc17"},
+    command = "RMCAirborn1stLT",
+    max = 0,
+    salary = 100,
+    admin = 0,
+    vote = false,
+    hasLicense = true,
+    candemote = false,
+    category = "RMC",
+    PlayerSpawn = function(ply)
+        ply:SetMaxHealth(170)
+        ply:SetHealth(170)
+        ply:SetArmor(170)
+    end
+})
+
+TEAM_CLOASS = DarkRP.createJob("RMCJumptrooperCPT", {
+    color = Color(0, 0, 0, 255),
+    model = {"models/fisher/ctna/trooper/rmc_jump/rmc_jump_trooper.mdl", "models/DxN/cod_ghosts/hazmat_pm.mdl", "models/defcon/banks/coldwweather/74th_cold/74th_trooper/74th_trooper.mdl", "models/smitty/bf2_reg/medic_officer/medic_officer.mdl"},
+    description = [[RMCJumpCPT]],
+    weapons = {"cross_arms_swep", "zbl_spray", "zbl_gun", "cross_arms_infront_swep", "rw_sw_dual_dc17", "weapon_cuff_elastic", "surrender_animation_swep", "salute_swep", "weapon_fists", "re_hands" ,"weapon_bactainjector" ,"weapon_medkit" ,"jet_mk1" ,"sw_datapad" ,"tfa_defi_swrp" ,"rw_sw_nade_bacta" ,"rw_ammo_distributor" ,"rw_sw_dc15a_o" ,"rw_sw_dc15s" ,"rw_sw_dc17"},
+    command = "RMCAirbornCPT",
+    max = 0,
+    salary = 100,
+    admin = 0,
+    vote = false,
+    hasLicense = true,
+    candemote = false,
+    category = "RMC",
+    PlayerSpawn = function(ply)
+        ply:SetMaxHealth(180)
+        ply:SetHealth(180)
+        ply:SetArmor(180)
+    end
+})
