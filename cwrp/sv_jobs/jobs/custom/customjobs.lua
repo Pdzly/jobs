f2e4dff6337a41343c1d341f0073cbe12fddfb47
Pdzly <34943569+Pdzly@users.noreply.github.com>
2021-07-01 17:21:37 +0200

    Update customjobs.lua

--- cwrp/sv_jobs/jobs/custom/customjobs.lua
+++ cwrp/sv_jobs/jobs/custom/customjobs.lua
@@ -367,11 +367,11 @@ TEAM_AARC88Snoke = DarkRP.createJob("A-ARC SGT 55 Deadschoot", {
     end
 })
 
-TEAM_vale = DarkRP.createJob("RC-Vale", {
+TEAM_RIGGS = DarkRP.createJob("RC-RIGGS", {
     color = Color(145, 7, 7, 255),
     model = {"models/defcon/loudmantis/shadows/vale.mdl"},
     description = [[RC_Vale]],
-    weapons = {"at_sw_dc15sa", "at_sw_dc17m", "bkeycard", "weapon_cuff_elastic", "rw_sw_nade_thermal", "tfa_defi_swrp", "weapon_thehiddenblade", "weapon_bactainjector", "weapon_armorkit", "cross_arms_infront_swep", "cross_arms_swep", "salute_swep", "seal6-c4", "hacktool", "comlink_swep", "alydus_fortificationbuildertablet"},
+    weapons = {"at_sw_dc15sa", "at_sw_dc17m", "bkeycard", "weapon_cuff_elastic", "rw_sw_nade_thermal", "tfa_defi_swrp", "weapon_thehiddenblade", "weapon_bactainjector", "weapon_armorkit", "cross_arms_infront_swep", "cross_arms_swep", "salute_swep", "seal6-c4", "hacktool", "comlink_swep", "alydus_fortificationbuildertablet", "weapon_breachingcharge", "t3m4_empgrenade", "defuse_kit", "rw_sw_nade_thermal"},
     command = "riggs",
     max = 1,
     salary = 250,
