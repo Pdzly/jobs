923e9845ea0663e8f7ad0eb4ef4fe0a16985d1e6
karlupo <73790544+karlupo@users.noreply.github.com>
2021-07-12 09:00:42 +0200

    karlupüo

--- cwrp/sv_jobs/jobs/212th/212th2ndab.lua
+++ cwrp/sv_jobs/jobs/212th/212th2ndab.lua
@@ -5,7 +5,7 @@ TEAM_2NDAIRBORNPFC = DarkRP.createJob("2nd Airborn PFC", {
 		"models/defcon/banks/coldwweather/212th_cold/212th_trooper/212th_trooper.mdl"
 	},
     description = [[2nd Airborn PVT]],
-    weapons = {"re_hands", "at_sw_dc15s_all", "rw_sw_dc15a", "weapon_fists", "jet_mk1", "cross_arms_infront_swep", "cross_arms_swep", "salute_swep"},
+    weapons = {"re_hands", "at_sw_dc15s_all", "rw_sw_dc15a", "weapon_fists", "jet_mk1", "cross_arms_infront_swep", "cross_arms_swep", "rw_sw_dc17ext", "salute_swep"},
     command = "2ndairbornpfc",
     max = 0,
     salary = 100,
@@ -28,7 +28,7 @@ TEAM_2NDAIRBORNLCL = DarkRP.createJob("2nd Airborn LCL", {
 		"models/defcon/banks/coldwweather/212th_cold/212th_trooper/212th_trooper.mdl"
 	},
     description = [[2nd Airborn LCL]],
-    weapons = {"re_hands", "at_sw_dc15s_all", "rw_sw_dc15a", "weapon_fists", "jet_mk1", "rw_sw_rps6", "rw_sw_dc17", "cross_arms_infront_swep", "cross_arms_swep", "salute_swep", "rw_sw_nade_thermal"},
+    weapons = {"re_hands", "at_sw_dc15s_all", "rw_sw_dc15a", "weapon_fists", "jet_mk1", "rw_sw_rps6", "rw_sw_dc17ext", "cross_arms_infront_swep", "cross_arms_swep", "salute_swep", "rw_sw_nade_thermal"},
     command = "2ndairbornlcl",
     max = 8,
     salary = 100,
@@ -51,7 +51,7 @@ TEAM_2NDAIRBORNCPL = DarkRP.createJob("2nd Airborn CPL", {
 		"models/defcon/banks/coldwweather/212th_cold/212th_trooper/212th_trooper.mdl"
 	},
     description = [[2nd Airborn CPL]],
-    weapons = {"re_hands", "at_sw_dc15s_all", "rw_sw_dc15a", "weapon_fists", "jet_mk1", "rw_sw_rps6", "rw_sw_dc17", "cross_arms_infront_swep", "cross_arms_swep", "salute_swep", "rw_sw_nade_thermal"},
+    weapons = {"re_hands", "at_sw_dc15s_all", "rw_sw_dc15a", "weapon_fists", "jet_mk1", "rw_sw_rps6", "rw_sw_dc17ext", "cross_arms_infront_swep", "cross_arms_swep", "salute_swep", "rw_sw_nade_thermal"},
     command = "2ndairborncpl",
     max = 8,
     salary = 100,
@@ -74,7 +74,7 @@ TEAM_2NDAIRBORNSGT = DarkRP.createJob("2nd Airborn SGT", {
 		"models/defcon/banks/coldwweather/212th_cold/212th_trooper/212th_trooper.mdl"
 	},
     description = [[2nd Airborn SGT]],
-    weapons = {"re_hands", "at_sw_dc15s_all", "rw_sw_dc15a", "weapon_fists", "jet_mk1", "rw_sw_rps6", "rw_sw_dc17", "cross_arms_infront_swep", "cross_arms_swep", "salute_swep", "rw_sw_nade_thermal"},
+    weapons = {"re_hands", "at_sw_dc15s_all", "rw_sw_dc15a", "weapon_fists", "jet_mk1", "rw_sw_rps6", "rw_sw_dc17ext", "cross_arms_infront_swep", "cross_arms_swep", "salute_swep", "rw_sw_nade_thermal"},
     command = "2ndairbornsgt",
     max = 5,
     salary = 100,
@@ -97,7 +97,7 @@ TEAM_2NDAIRBORNSGTMJ = DarkRP.createJob("2nd Airborn SGTMJ", {
 		"models/defcon/banks/coldwweather/212th_cold/212th_trooper/212th_trooper.mdl"
 	},
     description = [[2nd Airborn SGTMJ]],
-    weapons = {"re_hands", "at_sw_dc15s_all", "rw_sw_dc15a", "weapon_fists", "jet_mk1", "rw_sw_rps6", "rw_sw_dc17", "cross_arms_infront_swep", "cross_arms_swep", "salute_swep", "rw_sw_nade_thermal"},
+    weapons = {"re_hands", "at_sw_dc15s_all", "rw_sw_dc15a", "weapon_fists", "jet_mk1", "rw_sw_rps6", "rw_sw_dc17ext", "cross_arms_infront_swep", "cross_arms_swep", "salute_swep", "rw_sw_nade_thermal"},
     command = "2ndairbornsgtmj",
     max = 2,
     salary = 100,
@@ -120,7 +120,7 @@ TEAM_2NDAIRBORN2ndlt = DarkRP.createJob("2nd Airborn 2ndLT", {
 		"models/defcon/banks/coldwweather/212th_cold/212th_trooper/212th_trooper.mdl"
 	},
     description = [[2nd Airborn 2ndLT]],
-    weapons = {"re_hands", "at_sw_dc15s_all", "rw_sw_dc15a", "weapon_fists", "jet_mk1", "rw_sw_rps6", "rw_sw_dc17", "cross_arms_infront_swep", "cross_arms_swep", "salute_swep", "rw_sw_nade_thermal", "weapon_cuff_elastic"},
+    weapons = {"re_hands", "at_sw_dc15s_all", "rw_sw_dc15a", "weapon_fists", "jet_mk1", "rw_sw_rps6", "rw_sw_dc17ext", "cross_arms_infront_swep", "cross_arms_swep", "salute_swep", "rw_sw_nade_thermal", "weapon_cuff_elastic"},
     command = "2ndairborn2ndlt",
     max = 5,
     salary = 100,
@@ -143,7 +143,7 @@ TEAM_2NDAIRBORN1stLT = DarkRP.createJob("2nd Airborn 1stLT", {
 		"models/defcon/banks/coldwweather/212th_cold/212th_trooper/212th_trooper.mdl"
 	},
     description = [[2nd Airborn 1stLT]],
-    weapons = {"re_hands", "at_sw_dc15s_all", "rw_sw_dc15a", "weapon_fists", "jet_mk1", "rw_sw_rps6", "rw_sw_dc17", "cross_arms_infront_swep", "cross_arms_swep", "salute_swep", "rw_sw_nade_thermal", "weapon_cuff_elastic"},
+    weapons = {"re_hands", "at_sw_dc15s_all", "rw_sw_dc15a", "weapon_fists", "jet_mk1", "rw_sw_rps6", "rw_sw_dc17ext", "cross_arms_infront_swep", "cross_arms_swep", "salute_swep", "rw_sw_nade_thermal", "weapon_cuff_elastic"},
     command = "2ndairborn1stlt",
     max = 1,
     salary = 100,
@@ -167,7 +167,7 @@ TEAM_2NDAIRBORNCPT = DarkRP.createJob("2nd Airborn CPT", {
 		"models/starwars/grady/212th_airborne/212th_airborne2.mdl"
 	},
     description = [[2nd Airborn CPT]],
-    weapons = {"re_hands", "at_sw_dc15s_all", "rw_sw_dc15a", "weapon_fists", "jet_mk1", "rw_sw_rps6", "rw_sw_dual_dc17", "cross_arms_infront_swep", "cross_arms_swep", "salute_swep", "rw_sw_nade_thermal", "weapon_cuff_elastic"},
+    weapons = {"re_hands", "at_sw_dc15s_all", "rw_sw_dc15a", "weapon_fists", "jet_mk1", "rw_sw_rps6", "rw_sw_dual_dc17ext", "cross_arms_infront_swep", "cross_arms_swep", "salute_swep", "rw_sw_nade_thermal", "rw_sw_dc17ext", "weapon_cuff_elastic"},
     command = "2ndairborncpt",
     max = 1,
     salary = 100,
@@ -190,7 +190,7 @@ TEAM_2NDAIRBORNMJR = DarkRP.createJob("2nd Airborn MJR", {
 		"models/defcon/banks/coldwweather/212th_cold/212th_trooper/212th_trooper.mdl"
 	},
     description = [[2nd Airborn MJR]],
-    weapons = {"re_hands", "at_sw_dc15s_all", "rw_sw_dc15a", "weapon_fists", "jet_mk1", "rw_sw_rps6", "rw_sw_dual_dc17", "cross_arms_infront_swep", "cross_arms_swep", "salute_swep", "rw_sw_nade_thermal", "weapon_cuff_elastic"},
+    weapons = {"re_hands", "at_sw_dc15s_all", "rw_sw_dc15a", "weapon_fists", "jet_mk1", "rw_sw_rps6", "rw_sw_dual_dc17ext", "cross_arms_infront_swep", "rw_sw_dc17ext", "cross_arms_swep", "salute_swep", "rw_sw_nade_thermal", "weapon_cuff_elastic"},
     command = "2ndairbornmjr",
     max = 1,
     salary = 100,
