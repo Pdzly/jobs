033847423a65c3be7fb6506c162525df8e4ca020
Augukunn44 <87604778+Augukunn44@users.noreply.github.com>
2021-07-18 14:30:11 +0200

    Update jumptrooper.lua

--- cwrp/sv_jobs/jobs/501st/jumptrooper.lua
+++ cwrp/sv_jobs/jobs/501st/jumptrooper.lua
@@ -44,15 +44,37 @@ TEAM_501STJUMPPFC = DarkRP.createJob("501st Jumptrooper PFC", {
         ply:SetArmor(100)
     end
 })
-TEAM_501STJUMPPVT = DarkRP.createJob("501st Jumptrooper PVT", {
+TEAM_501STJUMPLCL = DarkRP.createJob("501st Jumptrooper LCL", {
     color = Color(0, 0, 255, 255),
     model = {
 		"models/defcon/loudmantis/501/jumptrooper.mdl",
 		
 	},
-    description = [[501stJUMPPVT]],
+    description = [[501stJUMPLCL]],
     weapons = {"weapon_fists", "re_hands",  "at_sw_dc15s_all", "at_sw_dc15a_all", "weapon_fists", "rw_sw_dc17ext", "cross_arms_infront_swep", "cross_arms_swep", "salute_swep", "jet_mk2"},
-    command = "501stjumppvt",
+    command = "501stjumpLCL",
+    max = 0,
+    salary = 100,
+    admin = 0,
+    vote = false,
+    hasLicense = true,
+    candemote = false,
+    category = "501st",
+    PlayerSpawn = function(ply)
+        ply:SetMaxHealth(200)
+        ply:SetHealth(200)
+        ply:SetArmor(100)
+    end
+})
+TEAM_501STJUMPCPL = DarkRP.createJob("501st Jumptrooper CPL", {
+    color = Color(0, 0, 255, 255),
+    model = {
+		"models/defcon/loudmantis/501/jumptrooper.mdl",
+		
+	},
+    description = [[501stJUMPCPL]],
+    weapons = {"weapon_fists", "re_hands",  "at_sw_dc15s_all", "at_sw_dc15a_all", "weapon_fists", "rw_sw_dc17ext", "cross_arms_infront_swep", "cross_arms_swep", "salute_swep", "jet_mk2"},
+    command = "501stjumpcpl",
     max = 0,
     salary = 100,
     admin = 0,
