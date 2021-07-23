d4eb8094a0ec56cac0f8070f489b7baa8e7dce84
Pdzly <34943569+Pdzly@users.noreply.github.com>
2021-07-23 21:13:12 +0200

    Create sh_config.lua

---
+++ cwrp/ammobox/lua/vs/ammobox/sh_config.lua
@@ -0,0 +1,10 @@
+mk_ammobox = mk_ammobox or {}
+
+mk_ammobox.config = {
+    kistemdl = "models/reizer_props/srsp/sci_fi/crate_01/crate_01.mdl", -- nicht ändern danke!! wegem Textscreen!
+    delay = 1,
+    clips = 25,
+    maxclips = 50,
+    refiltime = 1, -- in sekunden!
+    text = false,
+}
