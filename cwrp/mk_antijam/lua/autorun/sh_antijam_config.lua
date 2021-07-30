05f7a612d60b1b3c6dfe508d8a0a4b592df7d2e0
Pdzly <34943569+Pdzly@users.noreply.github.com>
2021-07-30 11:37:34 +0200

    Create sh_antijam_config.lua

---
+++ cwrp/mk_antijam/lua/autorun/sh_antijam_config.lua
@@ -0,0 +1,9 @@
+mk_jam = mk_jam or {}
+
+mk_jam.config = {
+    ["default"] = {
+        jamable = false, -- ja gut ;)
+        jamrate = 0.01, -- Die rate bei 100% Heat bsp.: jeder 100st Schuss wird 100% jamen
+        jamheat = 0.01, -- Die Hitze die pro Schuss auf kommt
+    }
+}
